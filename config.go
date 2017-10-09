a557e0ba648dc8cc0f6e532e49ec9326d847088e
Jinzhu <wosmvp@gmail.com>
2017-10-09 10:52:22 +0800

    Add Roler to Resource Config

--- config.go
+++ config.go
@@ -6,10 +6,11 @@ import "github.com/qor/roles"
 type Config struct {
 	Name       string
 	Menu       []string
-	Invisible  bool
-	Priority   int
-	PageCount  int
-	Singleton  bool
+	Roler      func(data interface{}, context *Context) []string
 	Permission *roles.Permission
 	Themes     []ThemeInterface
+	Priority   int
+	Singleton  bool
+	Invisible  bool
+	PageCount  int
 }
